bc6112a87f22a0276a808d0743a2c2e5feeb9dd9
H5K3 <bumpybr617@gmail.com>
2019-07-05 13:30:36 +0000

    kadai-tasklist-login1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
 before_action :set_task, only: [:show, :edit, :update, :destroy]
+before_action :require_user_logged_in
+
 def index
-  @task = Task.all
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
 end
+
 
 def show
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+  get 'microposts/destroy'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
+ 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
+  
   resources :tasks
 end
